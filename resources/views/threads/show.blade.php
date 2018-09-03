d60bbe5df2fcc1842f36494192d6d9d5892c2b02
Raihan S <raihan.act@gmail.com>
2018-09-03 20:55:58 +0600

    3-A-Thread-Can-Have-Replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -15,7 +15,11 @@
 
                         <ul>
                             @foreach($thread->replies as $reply)
-                                <li>{{$reply->body}}</li>
+                                <li>
+                                    <b><a href="#">{{$reply->owner->name}}</a>
+                                        said {{$reply->created_at->diffForHumans()}}</b>
+                                    {{$reply->body}}
+                                </li>
                             @endforeach
                         </ul>
                         <hr/>
